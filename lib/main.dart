90ad34ff9ed1bd0b705fde421f9de279ec256027
Debjit Chakraborty <zxyron0@gmail.com>
2020-06-17 15:45:45 +0530

    Dynamic theming Code from github.com/ResoCoder/flutter-theme-switching-bloc-tutorial

--- lib/main.dart
+++ lib/main.dart
@@ -1,45 +1,27 @@
 import 'package:flutter/material.dart';
-import 'package:pseudomusic/pages/splashcreen.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-void main() {
-  runApp(MyApp());
-}
+import 'ui/global/theme/bloc/bloc.dart';
+import 'ui/home/home_page.dart';
+
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => ThemeBloc(),
+      child: BlocBuilder<ThemeBloc, ThemeState>(
+        builder: _buildWithTheme,
+      ),
+    );
+  }
+
+  Widget _buildWithTheme(BuildContext context, ThemeState state) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Pseudo Music',
-      theme: ThemeData.light().copyWith(
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-        scaffoldBackgroundColor: Colors.white,
-        appBarTheme: AppBarTheme(
-          elevation: 0.0,
-          color: Colors.white,
-          iconTheme: IconThemeData(color: Colors.black),
-          textTheme: TextTheme(
-            headline6: TextStyle(
-                color: Colors.black,
-                fontWeight: FontWeight.bold,
-                fontSize: 20.0),
-          ),
-        ),
-      ),
-      darkTheme: ThemeData.dark().copyWith(
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-        canvasColor: Colors.black,
-        scaffoldBackgroundColor: Colors.black,
-        appBarTheme: AppBarTheme(color: Colors.black),
-        popupMenuTheme: PopupMenuThemeData(color: Colors.black),
-        bottomNavigationBarTheme: BottomNavigationBarThemeData(
-          backgroundColor: Colors.black,
-        ),
-        navigationRailTheme: NavigationRailThemeData(
-          backgroundColor: Colors.black
-        )
-      ),
-      home: SplashScreen(),
+      theme: state.themeData,
+      home: HomePage(),
     );
   }
 }
